5c00fcd90eb6d9d6bafcfc1b849c43560ec2ce2d
Daniel Downes <dd@danieldownes.co.uk>
2024-05-21 10:06:40 +0700

    Blank black screen!

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,32 @@
 import * as THREE from 'three'
+
+const width = window.innerWidth
+const height = window.innerHeight
+
+const renderer = new THREE.WebGLRenderer({
+	canvas: document.getElementById('app') as HTMLCanvasElement
+})
+renderer.setSize(width, height)
+
+const mainCamera = new THREE.PerspectiveCamera(60, width / height, 0.1, 100)
+
+//const scene = new BlasterScene(mainCamera)
+const scene = new THREE.Scene()
+
+//scene.initialize()
+
+function tick()
+{
+	//scene.update()
+	renderer.render(scene, mainCamera)
+	requestAnimationFrame(tick)
+}
+
+//init()
+tick()
+
+/*
+const gridHelper = new GridHelper(20, 20, 'teal', 'darkgray')
+    gridHelper.position.y = -0.01
+    scene.add(gridHelper)
+    */
