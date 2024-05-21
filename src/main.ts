6030a463c138df494158377ab4b2863d87d06f4f
Daniel Downes <dd@danieldownes.co.uk>
2024-05-21 10:18:30 +0700

    Cube added

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import { ThreeMFLoader } from 'three/examples/jsm/Addons.js'
 
 const width = window.innerWidth
 const height = window.innerHeight
@@ -10,10 +11,21 @@ renderer.setSize(width, height)
 
 const mainCamera = new THREE.PerspectiveCamera(60, width / height, 0.1, 100)
 
-//const scene = new BlasterScene(mainCamera)
 const scene = new THREE.Scene()
+const geo = new THREE.BoxGeometry()
+const mat = new THREE.MeshPhongMaterial({ color: 0xFFAA00 })
 
-//scene.initialize()
+const cube = new THREE.Mesh(geo, mat)
+
+cube.position.set(0, 1, -4)
+cube.setRotationFromEuler(new THREE.Euler(45, 45, 45))
+
+scene.add(cube)
+
+const light = new THREE.DirectionalLight(0xFFFFFF, 1)
+light.position.set(0, 4, 2)
+scene.add(light)
+
 
 function tick()
 {
